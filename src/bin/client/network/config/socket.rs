cc69c0850f0eb773f9f423ecef67a78d4fe63727
loshido <loshido@pm.me>
2024-12-12 22:44:00 +0100

    Wasm build fix

--- src/bin/client/network/config/socket.rs
+++ src/bin/client/network/config/socket.rs
@@ -1,6 +1,11 @@
 use aeronet_websocket::client::ClientConfig;
 
+#[cfg(target_family = "wasm")]
 pub fn config() -> ClientConfig {
-    ClientConfig::builder()
-        .with_no_cert_validation()
+    ClientConfig::default()
+}
+
+#[cfg(not(target_family = "wasm"))]
+pub fn config() -> ClientConfig {
+    ClientConfig::builder().with_no_cert_validation()
 }
